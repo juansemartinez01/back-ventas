82c6dc053f85b641268c118f5c3e31975f66685b
juansemartinez01 <juansemar123456789@gmail.com>
2025-06-30 12:41:44 -0300

    Estadistica producto promedio precio

--- src/estadisticas/estadisticas.service.ts
+++ src/estadisticas/estadisticas.service.ts
@@ -133,7 +133,7 @@ async getIngresosPorMesDesdeVista(desde: string, hasta: string) {
 
 
 async getPromedioGeneradoPorProducto() {
-  const query = `SET search_path TO public; SELECT * FROM promedio_precio_producto ORDER BY promedio_precio DESC`;
+  const query = `SELECT * FROM "public"."promedio_precio_producto" ORDER BY promedio_precio DESC`;
   return await this.dataSource.query(query);
 }
 
